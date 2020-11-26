523e809da003dba655b28ca4883de17f14b45fcb
MohamedElatroush <55201825+MohamedElatroush@users.noreply.github.com>
2020-11-26 10:43:43 +0200

    Add files via upload

--- Prototype1/main.py
+++ Prototype1/main.py
@@ -1,34 +1,47 @@
 import serial
-import re
+# import matplotlib as plt
 
-ser = serial.Serial('COM9') #check that port on your device from Device Manager
+ser = serial.Serial('COM9')
 inData=""
+indx = 0
+indx2 = 0
 time = []
 value = []
 counter = 0
+a=""
+b=""
 while(counter!=500):
- # x=ser.read(6).decode() # we have time and reading
- # print(x)
- x=ser.read(6).decode()
- inData+=x
- #print(inData)
+
+ x=ser.readline()
+ inData+=x.decode()
+ print(inData)
  for i in range(0,len(inData)):
   if(inData[i]=='T'):
-   print('T: ' +inData[i+1]+inData[i+2])
+   for j in range(i,len(inData)):
+    if(inData[j] != 'T' and inData[j] !='\n'):
+     a+=inData[j]
+   time.append(a)
+   indx = indx + 1
+   a=""
   elif(inData[i]=='R'):
-   print('R: ' +inData[i + 1] + inData[i + 2] + inData[i + 3]+ inData[i + 4])
+   for j in range(i,len(inData)):
+    if(inData[j]!='R' and inData[j]!='\n'):
+     b+=inData[j]
+   value.append(b)
+   indx2 = indx2 + 1
+   b=""
+
 
  counter=counter+1
  inData=""
 
 ser.close()
+print(time)
+print(value)
+print(len(time))
+print(len(value))
 
-#  if(x=='T'):
-#   y=serial.read(5).decode()
-#   time.append(y)
-#  counter=counter+1
-# print(time)
- #  if(x!='R'):
- #   x = ser.readline()  # we have time and reading
- #   print(x)
+# plt.plot(time,value)
+# plt.show()
+
 
